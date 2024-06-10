4b2daa4dca765d16a85ca6a7d9ec5ead267364ea
yogeshwaran1820 <yogeshwaran4714@gmail.com>
2024-06-10 16:42:15 +0530

    testcase changes

--- testcase/src/app/test/test.component.spec.ts
+++ testcase/src/app/test/test.component.spec.ts
@@ -1,21 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TestComponent } from './test.component';
+import { FormsModule } from '@angular/forms';
+import { SampleDirectiveDirective } from '../Directive/sample-directive.directive';
+import { HostBindingDirective } from '../Directive/host-binding.directive';
+import { TestServiceService } from '../test-service.service';
 
-describe('TestComponent', () => {
+fdescribe('TestComponent', () => {
   let component: TestComponent;
   let fixture: ComponentFixture<TestComponent>;
+  let service:TestServiceService
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TestComponent]
+      declarations: [TestComponent,SampleDirectiveDirective,HostBindingDirective],
+      imports: [FormsModule]
     });
     fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(TestServiceService)
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should call func method", ()=>{
+    spyOn(component,'func').and.callThrough();
+    component.func();
+    expect(component.func).toHaveBeenCalled()
+  })
 });
